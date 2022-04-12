cdddebc7f24d7d2828626d2f6924da68f9f0d85e
[firstname <[tuannaph14620@fpt.edu.vn]>
2022-04-12 16:55:06 +0700

    footer

--- src/components/FooterIndex.js
+++ src/components/FooterIndex.js
@@ -3,20 +3,20 @@ import React from 'react'
 const FooterIndex = () => {
   return (
     <footer className="pt-10">
-  <div className=" bg-blue-500">
+  <div className=" bg-blue-300  ">
     <div className="flex justify-between px-10 py-5 align-center">
       <div className>
         <ul className="flex">
-          <li><a className="pr-10" href="#">FAQ &amp; CONTACT</a></li>
-          <li><a className="pr-10" href="#">DELIVERY &amp; SHIPPING</a></li>
-          <li><a className="pr-10" href="#">MY ACCOUNT</a></li>
+          <li><a className="pr-10 text-lg font-bold text-blue-900" href="#">FAQ &amp; CONTACT</a></li>
+          <li><a className="pr-10 text-lg font-bold text-blue-900" href="#">DELIVERY &amp; SHIPPING</a></li>
+          <li><a className="pr-10 text-lg font-bold text-blue-900" href="#">MY ACCOUNT</a></li>
         </ul>
       </div>
       <div className="icon_social">
-        <a style={{color: 'black'}} href="https://www.facebook.com/mancity"><i className="fab fa-facebook-square text-3xl" /></a>
-        <a style={{color: 'black'}} href="https://www.youtube.com/user/mcfcofficial"><i className="fab fa-youtube-square text-3xl" /></a>
-        <a style={{color: 'black'}} href="https://www.instagram.com/mancity/"><i className="fab fa-instagram-square text-3xl" /></a>
-        <a style={{color: 'black'}} href="https://mobile.twitter.com/ManCity"><i className="fab fa-twitter-square text-3xl" /></a>
+        <a style={{color: 'white'}} href="https://www.facebook.com/mancity"><i className="fab fa-facebook-square text-3xl mr-5" /></a>
+        <a style={{color: 'white'}} href="https://www.youtube.com/user/mcfcofficial"><i className="fab fa-youtube-square text-3xl mr-5" /></a>
+        <a style={{color: 'white'}} href="https://www.instagram.com/mancity/"><i className="fab fa-instagram-square text-3xl mr-5" /></a>
+        <a style={{color: 'white'}} href="https://mobile.twitter.com/ManCity"><i className="fab fa-twitter-square text-3xl mr-5" /></a>
       </div>
     </div>
   </div>
